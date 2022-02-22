e204026b9ef2e6d7ba214fb4daae9dfb529563b0
judith lourde <judith.lourde@gmail.com>
2022-02-22 09:06:33 +0100

    Add logo to Header

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <header class="header">
+        <div class="header__logo">
+            <img src="/images/svg/logo.svg" alt="logo-yummy">
+        </div>
+
+        <div class="header__hamburger">
+            <img src="/images/svg/hamburger.svg" alt="hamburger-icon">
+        </div>
+    </header>
+</template>
+
+<style>
+    .header {
+        padding: 0 var(--top-small) 0 var(--top-small);
+        background-color: var(--header);
+        box-shadow: 0 0 5px 0;
+        height: 80px;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        /* position: fixed;
+        top: 0;
+        left: 0; */
+    }
+
+    .header__logo {
+    
+    }
+
+    .header__logo img {
+        /* width: 18%; */
+    }
+</style>
