d4c81e40d2b9209bbe8e36c5f62a0d503e41d180
judith lourde <judith.lourde@gmail.com>
2022-02-22 09:07:31 +0100

    Change name Home to HomeView in views

---
+++ src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+    <!-- <div class="home"> -->
+        <Header />
+        <Footer />
+    <!-- </div> -->
+    
+</template>
+
+<script>
+import Header from './Header.vue';
+import Footer from './Footer.vue';
+    export default {
+        components: {
+            Header,
+            Footer,
+
+        },
+
+        data() {
+            
+        },
+    }
+</script>
+
+<style>
+    .home {
+        position: relative;
+        /* width: 100%; */
+        /* height: 100%; */
+    }
+</style>
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+	<Home />
+</template>
+
+<script>
+import Home from '../components/Home.vue';
+	export default {
+		components: {
+            Home
+        }, 
+		data() {
+			clientID: import.meta.env.VITE_APP_NAME
+		}
+	}
+</script>
+
+
