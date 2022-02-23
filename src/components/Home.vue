65fca0e03a4dc95cc6b8d1175e310d3f271df11a
judith lourde <judith.lourde@gmail.com>
2022-02-23 01:00:52 +0100

    Add components to Home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,28 @@
 <template>
-    <!-- <div class="home"> -->
-        <Header />
-        <Footer />
-    <!-- </div> -->
-    
+    <section class="home">
+        <!-- <header class="home__header"> -->
+            <Header />
+        <!-- </header> -->
+
+        <!-- <main class="home__main"> -->
+            <Main />
+        <!-- </main> -->
+            
+        <!-- <footer class="home__footer"> -->
+            <Footer />
+        <!-- </footer>  -->
+    </section>  
 </template>
 
 <script>
 import Header from './Header.vue';
+import Main from './Main.vue';
 import Footer from './Footer.vue';
     export default {
         components: {
             Header,
+            Main,
             Footer,
-
         },
 
         data() {
@@ -24,9 +33,16 @@ import Footer from './Footer.vue';
 
 <style>
     .home {
-        position: relative;
-        /* width: 100%; */
-        /* height: 100%; */
+        width: 100vw;
+        height: 100vh;
+        /* display: grid;
+        grid-template-rows: 100px 1fr 100px; */
+        row-gap: 0;
     }
+
+    .home__header {
+
+    }
+    
 </style>
 
