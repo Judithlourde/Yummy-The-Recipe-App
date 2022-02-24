bf4b98e094933d4ce7b49a11abb9268c86e75a5d
judith lourde <judith.lourde@gmail.com>
2022-02-24 02:03:38 +0100

    Change Component name

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <Header />
+  	<Main />
+    <Footer />
+</template>
+
+<script>
+import Header from '../components/Header.vue';
+import Main from '../components/Main.vue';
+import Footer from '../components/Footer.vue';
+    export default {
+        components: {
+            Header,
+            Main,
+            Footer,
+        },
+
+        data() {
+			clientID: import.meta.env.VITE_APP_NAME
+		}
+    }
+</script>
+
+
+
+
