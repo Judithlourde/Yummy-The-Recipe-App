b724edd508cecdd6748f9955648822564c576407
judith lourde <judith.lourde@gmail.com>
2022-03-21 23:13:49 +0100

    Fix social icon size to responsive

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -24,11 +24,28 @@
         align-items: center;
     }
 
+    .footer__logo {
+        width: 30%;
+    }
+
     .footer__social-icons {
         display: flex;
     }
 
     .footer__social-icons img {
-        padding: 0 var(--top-small) 0 var(--top-small);
+        padding: 0 var(--top-small) 0 0;
+        width: 25%;
+    }
+
+    /* Medium screen devices (968px and above) */
+    @media screen and (min-width: 968px) {
+        .footer__logo {
+            width: 10%;
+        }
+
+        .footer__social-icons img {
+            padding: 0 var(--top-xsmall) 0 var(--top-xsmall);
+            width: 30%;
+        }
     }
 </style>
